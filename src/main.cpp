ba4d11fcf35759b56648f3b183639931e1404882
Xusniddin Qalandarov <20221400182@newuzbekistanuniversity.uz>
2023-10-17 12:12:16 +0500

    1st

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,16 @@
  * Name:
  */
 
+
 #include <iostream>
 #include "task_1.h"
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    // Create objects of the Family class
+    Family myFamily("John", 40, "Jane", 35, "Alex", 10);
+
+    // Call the displayFamilyMembers function
+    myFamily.displayFamilyMembers();
+
     return 0;
 }
--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,36 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Xusniddin Qalandarov
+ * Date: 10/17/2024
+ * Name: Xusniddin Qalandarov
  */
+
+#include <iostream>
+#include <string>
+
+class Family {
+public:
+    Family(std::string father, int fatherAge, std::string mother, int motherAge, std::string child, int childAge) {
+        fatherName = father;
+        motherName = mother;
+        childName = child;
+        this->fatherAge = fatherAge;
+        this->motherAge = motherAge;
+        this->childAge = childAge;
+    }
+    void displayFamilyMembers() {
+        std::cout << "Family Members:\n";
+        std::cout << "Father: " << fatherName << ", Age: " << fatherAge << "\n";
+        std::cout << "Mother: " << motherName << ", Age: " << motherAge << "\n";
+        std::cout << "Child: " << childName << ", Age: " << childAge << "\n";
+    }
+
+public:
+    std::string fatherName;
+    std::string motherName;
+    std::string childName;
+
+private:
+    int fatherAge;
+    int motherAge;
+    int childAge;
+};
